b7d23ab0018765cb221ea783f29764d45658d1ae
Parthiban Loganathan <pl2487@columbia.edu>
2013-08-03 18:51:47 -0400

    Update parser

--- pTagsParser.js
+++ pTagsParser.js
@@ -1,11 +1,18 @@
-$('p').each(function() {
-  var rand = Math.random() * 10;
+jQuery.fn.splitBySentence() = function(difficulty) {
+  $('p').each(function() {
+    var rand = Math.random() * 10;
 
-  if (rand < 2) {
-    var sentence = $(this).modifyParagraph('easy');
-    $(this).replace(sentence, "<span class='facebook_translate' data-original=\"" + sentence + "\">" + sentence + "</span>");
-  }
+    if (rand < difficulty) {
+      var sentence = $(this).getSentence('easy');
+      $(this).replace(sentence, "<span class='facebook_translate' data-original=\"" + sentence + "\">" + sentence + "</span>");
+    }
 
-});
+  });
+};
 
-
+jQuery.fn.splitByWord() = function() {
+  $('p').each(function() {
+      var word = $(this).getWord('easy');
+      $(this).replace(word, "<span class='facebook_translate' data-original=\"" + word + "\">" + word + "</span>");
+  });
+};
--- parser.js
+++ parser.js
@@ -1,3 +1,36 @@
+jQuery.fn.getWord() = function(mode) {
+
+  // split paragraph into sentences
+  var text = $(this).text();
+  var words = text.split(" ");
+
+  // sort by length
+  words.sort(function(a, b) {
+    return a.length - b.length;
+  });
+
+  // return num words weighted based on difficulty
+  var weight = [];
+
+  if (mode == 'random') {
+    for (var i = 0; i < words.length; i++) {
+      weight.push(1);
+    }
+  } else if (mode == 'easy') {
+    for (var i = 0; i < words.length; i++) {
+      weight.push(words[i].length*10);
+    }
+  } else if (mode == 'hard') {
+    for (var i = words.length; i >= 0; i--) {
+      weight.push(words[i].length*10);
+    }
+  }
+
+  var result = getRandomItem(words, weight);
+
+  return result;
+};
+
 jQuery.fn.getSentence() = function(mode) {
 
   // split paragraph into sentences
@@ -18,20 +51,20 @@ jQuery.fn.getSentence() = function(mode) {
     }
   } else if (mode == 'easy') {
     for (var i = 0; i < sentences.length; i++) {
-      weight.push(sentences[i].length);
+      weight.push(sentences[i].length*10);
     }
   } else if (mode == 'hard') {
     for (var i = sentences.length; i >= 0; i--) {
-      weight.push(sentences[i].length);
+      weight.push(sentences[i].length*10);
     }
   }
 
-  var result = getRandomItems(sentences, weight);
+  var result = getRandomItem(sentences, weight);
 
   return result;
 };
 
-function getRandomItems(list, weight) {
+function getRandomItem(list, weight) {
   var total_weight = weight.reduce(function (prev, cur, i, arr) {
     return prev + cur;
   });
