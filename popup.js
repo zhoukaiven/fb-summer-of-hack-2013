190f07bced383029945187281a83d0d28c389f47
Parthiban Loganathan <pl2487@columbia.edu>
2013-08-04 07:18:34 -0400

    Add sound

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	chrome.storage.sync.get(['status', 'lang', 'diff'], function(data){
+	chrome.storage.sync.get(['status', 'volume', 'lang', 'diff'], function(data){
 		data.status = data.status ? data.status : "on";
 		$("#status").val(data.status);
 		chrome.storage.sync.set({'status' : data.status})
@@ -7,7 +7,11 @@ $(document).ready(function () {
 		data.lang = data.lang ? data.lang : "ar";
 		$("#lang").val(data.lang);
 		chrome.storage.sync.set({'lang' : data.lang})
-		
+	
+          	data.volume = data.volume ? data.volume : "0";
+		$("#volume").val(data.volume);
+		chrome.storage.sync.set({'volume' : data.volume})
+
 		data.diff = data.diff ? data.diff : "1";
 		$("#diff").val(data.diff);
 		chrome.storage.sync.set({'diff' : data.diff})	
@@ -26,6 +30,12 @@ $(document).ready(function () {
 	});
 	$("#lang").change(function(){
 		chrome.storage.sync.set({'lang': $(this).val() });
+		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+			chrome.tabs.reload(tabs[0]['id']);
+		});
+	});
+        $("#volume").change(function(){
+		chrome.storage.sync.set({'volume': $(this).val() });
 		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
 			chrome.tabs.reload(tabs[0]['id']);
 		});
