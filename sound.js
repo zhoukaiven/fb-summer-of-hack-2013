1fef46185f15d1e8d68769f6f6e5a3fd02739658
Parthiban Loganathan <pl2487@columbia.edu>
2013-08-04 04:06:42 -0400

    Updates

--- sound.js
+++ sound.js
@@ -1,4 +1,5 @@
 function playSpeech(var text) {
+  alert("Called");
   $('#Video-Source').attr('src', "http://translate.google.com/translate_tts?tl=en&q=" + encodeUriComponent(text));
   var v = $("#Video")[0];
   v.load();
