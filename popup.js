4d76801577722d8cde47d9261a1230f7e870ffd6
Bob Ren <bob@codecademy.com>
2013-08-04 07:34:25 -0400

    volume control

--- popup.js
+++ popup.js
@@ -3,25 +3,25 @@ $(document).ready(function () {
 		data.status = data.status ? data.status : "on";
 		$("#status").val(data.status);
 		chrome.storage.sync.set({'status' : data.status})
-		
+
 		data.lang = data.lang ? data.lang : "ar";
 		$("#lang").val(data.lang);
 		chrome.storage.sync.set({'lang' : data.lang})
-	
-          	data.volume = data.volume ? data.volume : "0";
+
+        data.volume = data.volume ? data.volume : "0";
 		$("#volume").val(data.volume);
 		chrome.storage.sync.set({'volume' : data.volume})
 
 		data.diff = data.diff ? data.diff : "1";
 		$("#diff").val(data.diff);
-		chrome.storage.sync.set({'diff' : data.diff})	
+		chrome.storage.sync.set({'diff' : data.diff})
 	});
 
     $('.advanced-settings-button').click(function () {
         $('.normal-settings').toggleClass('show');
         $('.advanced-settings').toggleClass('show');
     });
-	
+
 	$("#status").change(function(){
 		chrome.storage.sync.set({'status': $(this).val() });
 		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
@@ -34,11 +34,11 @@ $(document).ready(function () {
 			chrome.tabs.reload(tabs[0]['id']);
 		});
 	});
-        $("#volume").change(function(){
-		chrome.storage.sync.set({'volume': $(this).val() });
-		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-			chrome.tabs.reload(tabs[0]['id']);
-		});
+    $("#volume").change(function(){
+    	chrome.storage.sync.set({'volume': $(this).val() });
+    	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    		chrome.tabs.reload(tabs[0]['id']);
+    	});
 	});
 	$("#diff").change(function(){
 		chrome.storage.sync.set({'diff': $(this).val() });
