b35cdbbce5d0f28fae30cf75014dd39f42d81c70
Parthiban Loganathan <pl2487@columbia.edu>
2013-08-03 22:02:05 -0400

    Adding stuff

--- hover.js
+++ hover.js
@@ -1,3 +1,16 @@
+// Create the tooltips only when document ready
+ $(document).ready(function()
+ {
+     // MAKE SURE YOUR SELECTOR MATCHES SOMETHING IN YOUR HTML!!!
+     $('translate_this2465').each(function() {
+         $(this).qtip({
+             content: {
+                 text: $(this).next('.tooltiptext')
+             }
+         });
+     });
+ });
+
 $('.translate_this2465').hover(function() {
   content = ($(this).attr('data-original'));
 });
