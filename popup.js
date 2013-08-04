ef45b85367ec794cd9b1f7a5c91a0bbf199defaf
zhoukaiven <zhou.kaiven@gmail.com>
2013-08-03 20:45:46 -0400

    user choices of normal settings persist

--- popup.js
+++ popup.js
@@ -1,6 +1,27 @@
 $(document).ready(function () {
+
+	chrome.storage.sync.get(['status', 'lang', 'diff'], function(data){
+		$("#status").val(data.status || "off") ;
+		$("#lang").val(data.lang || "ar");
+		$("#diff").val(data.diff || "easy");
+	});
+
     $('.advanced-settings-button').click(function () {
         $('.normal-settings').toggleClass('show');
         $('.advanced-settings').toggleClass('show');
     });
+	
+	$("#status").change(function(){
+		chrome.storage.sync.set({'status': $(this).val() });
+	});
+	$("#lang").change(function(){
+		chrome.storage.sync.set({'lang': $(this).val() });
+	});
+	$("#diff").change(function(){
+		chrome.storage.sync.set({'diff': $(this).val() });
+	});
+
+	
+
+
 });
