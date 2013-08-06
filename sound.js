a25340e9020873f850b574aae45979c5b10e46d4
Parthiban Loganathan <pl2487@columbia.edu>
2013-08-06 09:01:49 -0400

    Remove merged/unwanted files

--- sound.js
+++
@@ -1,6 +0,0 @@
-function playSpeech(text) {
-  $('#Video-Source').attr('src', "http://translate.google.com/translate_tts?tl=en&q=" + encodeURIComponent(text));
-  var v = $("#Video")[0];
-  v.load();
-  v.play();
-}
